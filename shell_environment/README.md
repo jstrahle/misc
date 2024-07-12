b91abf2f4dfe9b61fa2a585343da2f370b636f95
Jarkko Strahle <jstrahle@redhat.com>
2024-07-12 12:32:18 +0300

    added shell scripts and env files

---
+++ shell_environment/README.md
@@ -0,0 +1,4 @@
+Vimrc requires downloading pathogen:
+
+mkdir -p ~/.vim/autoload/
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
---
+++ shell_environment/bashrc.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+# Enable cli bastebin that can be used with pipe
+echo "alias pastah=\"curl -F 'j=<-' https://pastah.space\"" >> ~/.bashrc
